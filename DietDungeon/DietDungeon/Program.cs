241a775f9b5c3b102efce853bf1474d49867178a
ljh5945 <ljh594545@gmail.com>
2024-01-09 17:25:15 +0900

    주의사항 제거

--- DietDungeon/DietDungeon/Program.cs
+++ DietDungeon/DietDungeon/Program.cs
@@ -30,15 +30,6 @@
         {
             GameDataSetting();
             StartMenu();
-
-            Console.WriteLine("다이어트 던전 파일입니다!!!!");
-            Console.WriteLine("여기에서 코드를 합쳐 완성합시다");
-
-            Console.WriteLine("주의사항!!!!");
-            Console.WriteLine("1. 막 바꾸면 헷갈릴 수 있으니 역할을 정하고 미리 구현했을 땐 말씀해주세요!");
-            Console.WriteLine("같은줄에 코드가 곂치면 오류가 발생합니다!!");
-            Console.WriteLine("2. 한 명씩 푸쉬해야 합니다. 푸쉬하기 전에 말씀해주세요");
-            Console.WriteLine("푸쉬할 때 잘못 하면 오류가 발생합니다!!");
         }
 
         private static void StartMenu()
